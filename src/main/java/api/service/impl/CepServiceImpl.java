88aca16dec41ae086a4af3c3781840f6ac79a771
Tiago Moraes <tiagoj61@gmail.com>
2023-10-20 07:14:55 -0300

    feat: HTTP Client

--- src/main/java/api/service/impl/CepServiceImpl.java
+++ src/main/java/api/service/impl/CepServiceImpl.java
@@ -8,6 +8,7 @@ import api.service.CepService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -30,27 +31,29 @@ public class CepServiceImpl implements CepService {
     public CepResponseDto getAllCepsByName(String uf, String cidade, String name) {
         try {
             //HTTP Client
-            HttpClient client = HttpClient.newBuilder()
-                    .connectTimeout(Duration.ofSeconds(5))
-                    .build();
-
-            URI uri = URI.create("https://viacep.com.br/ws" + "/"+uf+"/"+cidade+"/"+name+"/json");
-
-            HttpRequest request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(uri)
-                    .build();
-
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
+            HttpResponse<String> response = getCep(uf, cidade, name);
 
             List<CepResponseDto> cepResponseDto = (List<CepResponseDto>) ConvertJson.convert(response.body(), CepResponseDto.class);
-
 
 
         } catch (Exception e) {
         }
         return null;
+    }
+
+    private HttpResponse<String> getCep(String uf, String cidade, String name) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
+
+        URI uri = URI.create("https://viacep.com.br/ws" + "/" + uf + "/" + cidade + "/" + name + "/json");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
     public void removeCeps(List<CepResponseDto> ceps, FilterCepsFunction function) {
